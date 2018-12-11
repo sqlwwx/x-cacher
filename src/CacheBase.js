1e794343cb0ef77e4be70429c1adf47fe5e673c4
sqlwwx <wwx_2012@hotmail.com>
2018-12-11 16:09:33 +0800

    refactor(redis): add doGet

--- src/CacheBase.js
+++ src/CacheBase.js
@@ -9,36 +9,31 @@ export default class CacheBase {
     return this.prefix + ':' + key
   }
 
-  async getAndSave (key, fn, ...expires) {
+  async doGet (alwaysSave, key, fn, expires) {
     const fullKey = this.buildKey(key)
     let ret = await this.loadFromCache(fullKey)
     if (!ret) {
       if (this.parent) {
-        ret = await this.parent.getAndSave(key, fn, ...expires.splice(1))
+        ret = await this.parent.doGet(alwaysSave, key, fn, expires.splice(1))
       } else if (fn) {
         ret = await fn(this)
       }
+      if (ret && !alwaysSave) {
+        await this.saveToCache(fullKey, ret, expires[0] || this.expire)
+      }
     }
-    if (ret) {
+    if (ret && alwaysSave) {
       await this.saveToCache(fullKey, ret, expires[0] || this.expire)
     }
     return ret
   }
 
+  async getAndSave (key, fn, ...expires) {
+    return this.doGet(true, key, fn, expires)
+  }
+
   async get (key, fn, ...expires) {
-    const fullKey = this.buildKey(key)
-    let ret = await this.loadFromCache(fullKey)
-    if (!ret) {
-      if (this.parent) {
-        ret = await this.parent.get(key, fn, ...expires.splice(1))
-      } else if (fn) {
-        ret = await fn(this)
-      }
-      if (ret) {
-        await this.saveToCache(fullKey, ret, expires[0] || this.expire)
-      }
-    }
-    return ret
+    return this.doGet(false, key, fn, expires)
   }
 
   async loadFromCache (fullKey) {
